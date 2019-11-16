0c65e751da3be300e2600d93c2aa47a70a6fc4a4
Callmehoiiii <thedangthanhh@gmail.com>
2019-11-17 00:25:41 +0700

    UI__update
    3 popups + sweetalert.js + sweetalert2.js
    link theo thu tu giong trong trang login.

---
+++ Project/BookStore/BookStore/wwwroot/js/sweetalert.js
@@ -0,0 +1,37 @@
+
+function popupSuccess() {
+    Swal.fire({
+        title: 'Success',
+        text: 'Do you want to continue',
+        icon: 'success',      
+      })
+}
+
+function popupFail() {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href>Why do I have this issue?</a>'
+    })
+}
+
+function popupConfirm() {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+        }
+    })
+}
