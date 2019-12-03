41f45457dff2c182ada93d36354b558235412685
Callmehoiiii <thedangthanhh@gmail.com>
2019-12-03 16:53:25 +0700

    UI__update
    statistic page

--- Project/BookStore/BookStore/wwwroot/js/chart.js
+++ Project/BookStore/BookStore/wwwroot/js/chart.js
@@ -1,10 +1,9 @@
-let myChart = document.getElementById('myChart').getContext('2d');
+let topSale = document.getElementById('topSale').getContext('2d');
 
-
-let massPopChart = new Chart(myChart, {
+let massPopChart = new Chart(topSale, {
     type: 'bar',
     data: {
-        labels: ['January', 'Feburary', 'March', 'April', 'May', 'June','July','August','September','October','November','December'],
+        labels: ['Book 1', 'Book 2', 'Book 3', 'Book 4', 'Book 5', 'Book 6', 'Book 7', 'Book 8', 'Book 9', 'Book 10'],
         datasets: [{
             label: 'Book sale',
             data: [
@@ -17,9 +16,7 @@ let massPopChart = new Chart(myChart, {
                 720,
                 670,
                 654,
-                582,
-                990,
-                762
+                582
             ],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
@@ -27,15 +24,15 @@ let massPopChart = new Chart(myChart, {
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',            
+                'rgba(255, 159, 64, 0.2)',
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'  
-                
-                
+                'rgba(255, 159, 64, 0.2)'
+
+
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -43,13 +40,13 @@ let massPopChart = new Chart(myChart, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',            
+                'rgba(255, 159, 64, 1)',
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'  
+                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
@@ -58,31 +55,31 @@ let massPopChart = new Chart(myChart, {
     options: {
         title: {
             display: true,
-            text: 'Book Sold by Month',
-            fontSize: 25,            
+            text: 'Top sale of this month',
+            fontSize: 25,
         },
-        legend:{
+        legend: {
             display: false,
-            position:'right',
-            labels:{
+            position: 'right',
+            labels: {
                 fontColor: '#000'
             }
         },
-        layout:{
-            padding:{
-                left:50,
-                right:0,
-                top:0,
-                bottom:0
+        layout: {
+            padding: {
+                left: 50,
+                right: 0,
+                top: 0,
+                bottom: 0
             }
         },
-        tooltips:{
+        tooltips: {
             enabled: true
         },
-        
-        scales:{
-            yAxes:[{
-                ticks:{
+
+        scales: {
+            yAxes: [{
+                ticks: {
                     beginAtZero: true
                 }
             }]
@@ -92,11 +89,12 @@ let massPopChart = new Chart(myChart, {
 
 });
 
-let myChart2 = document.getElementById('myChart2').getContext('2d');
-let massPopChart2 = new Chart(myChart2, {
+let saleMonthly = document.getElementById('saleMonthly').getContext('2d');
+
+let massPopChart2 = new Chart(saleMonthly, {
     type: 'line',
     data: {
-        labels: ['January', 'Feburary', 'March', 'April', 'May', 'June','July','August','September','October','November','December'],
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         datasets: [{
             label: 'Book sale',
             data: [
@@ -114,8 +112,8 @@ let massPopChart2 = new Chart(myChart2, {
                 762
             ],
             backgroundColor: [
-               'transparent'
-                
+                'transparent'
+
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -123,46 +121,45 @@ let massPopChart2 = new Chart(myChart2, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',            
+                'rgba(255, 159, 64, 1)',
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'  
+                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
-
     },
     options: {
         title: {
             display: true,
             text: 'Book Sold by Month',
-            fontSize: 25,            
+            fontSize: 25,
         },
-        legend:{
+        legend: {
             display: false,
-            position:'right',
-            labels:{
+            position: 'right',
+            labels: {
                 fontColor: '#000'
             }
         },
-        layout:{
-            padding:{
-                left:50,
-                right:0,
-                top:0,
-                bottom:0
+        layout: {
+            padding: {
+                left: 50,
+                right: 0,
+                top: 0,
+                bottom: 0
             }
         },
-        tooltips:{
+        tooltips: {
             enabled: true
         },
-        
-        scales:{
-            yAxes:[{
-                ticks:{
+
+        scales: {
+            yAxes: [{
+                ticks: {
                     beginAtZero: true
                 }
             }]
@@ -171,3 +168,4 @@ let massPopChart2 = new Chart(myChart2, {
     },
 
 });
+
